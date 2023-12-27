742830bf14980cf944fc6d30c65157c8621acf94
yashjawale <yash25.j@gmail.com>
2023-12-27 18:46:44 +0530

    Configure Tailwind & Fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+        sans: ['JetBrains Mono Variable', ...defaultTheme.fontFamily.sans], // Used by tailwind as default
+        title: ["Abril Fatface", "Inter", ...defaultTheme.fontFamily.sans],
+      },
+		},
 	},
 	plugins: [],
 }
