8fe2ad6dd385f2e0b4a33f4eb92988eb0a378e5f
Yash Jawale <yash25.j@gmail.com>
2024-01-16 11:59:18 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yashjawale.github.io',
-  base: '/acunetix-11-website',
   integrations: [tailwind()]
 });
